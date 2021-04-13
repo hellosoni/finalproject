75e3d845adf36ab9d64d1f1a7ee8f21d7c984eb5
nghi nguyen <nguyen.1759@osu.edu>
2021-04-13 12:37:44 -0400

    Add script for indexing reference genome

---
+++ scripts/starindex_submit.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#SBATCH --account=PAS1855
+#SBATCH --time=3:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=6
+#SBATCH --cpus-per-task=1
+#SBATCH --job-name STAR_index
+
+module load python/3.6-conda5.2
+source activate star-env
+
+STAR --runThreadN 28 \
+--runMode genomeGenerate \
+--genomeDir /fs/ess/PAS1855/users/nghinguyen/FinalProject/STARindexed_ref \
+--genomeFastaFiles /fs/ess/PAS1855/users/nghinguyen/FinalProject/refgenome/Gmax_275_v2.0.fa \
+--genomeSAindexNbases 13
+--sjdbOverhang 99
