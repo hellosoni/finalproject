ad3daadbecede43b83fcb5fd3cccd79b39fdcad1
nghi nguyen <nguyen.1759@osu.edu>
2021-04-18 17:05:34 -0400

    Modified scripts

--- scripts/adapter_trimming.sh
+++ scripts/adapter_trimming.sh
@@ -11,7 +11,7 @@ cd /fs/ess/PAS1855/users/nghinguyen/FinalProject/data_raw/
 for i in `ls -1 /fs/ess/PAS1855/users/nghinguyen/FinalProject/data_raw/*_1.fastq | sed 's/_1.fastq//'` 
 do
 
-/fs/ess/PAS1855/users/nghinguyen/FinalProject/softwares/bbmap/bbduk.sh -Xmx1g in1=$i\_1.fastq in2=$i\_2.fastq out1=/fs/ess/PAS1855/users/nghinguyen/FinalProject/results/trimmed/${i##*/}\_clean_1.fastq out2=/fs/ess/PAS1855/users/nghinguyen/FinalProject/results/trimmed/${i##*/}\_clean_2.fastq ref=/fs/ess/PAS1855/users/nghinguyen/FinalProject/softwares/bbmap/resources/adapters.fa
+/fs/ess/PAS1855/users/nghinguyen/FinalProject/softwares/bbmap/bbduk.sh -Xmx1g in1=$i\_1.fastq in2=$i\_2.fastq out1=/fs/ess/PAS1855/users/nghinguyen/FinalProject/results/trimmed/adapter_trimmed/${i##*/}\_clean_1.fastq out2=/fs/ess/PAS1855/users/nghinguyen/FinalProject/results/trimmed/adapter_trimmed/${i##*/}\_clean_2.fastq ref=/fs/ess/PAS1855/users/nghinguyen/FinalProject/softwares/bbmap/resources/adapters.fa
  ktrim=r k=23 mink=11 hdist=1 tpe tbo
 
 done
