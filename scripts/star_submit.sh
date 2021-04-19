3fb73c19d81b8f346d26a3fcb14a89104d6731cc
nghi nguyen <nguyen.1759@osu.edu>
2021-04-18 23:46:10 -0400

    Modified script

--- scripts/star_submit.sh
+++ scripts/star_submit.sh
@@ -7,6 +7,7 @@
 #SBATCH --cpus-per-task=1
 #SBATCH --job-name STAR_map
 
+cd /fs/ess/PAS1855/users/nghinguyen/FinalProject/results/STAR/STARres
 
 module load python/3.6-conda5.2
 source activate star-env
